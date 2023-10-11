b431c9dc75ede2d65bb987f06f76decb841e2bb1
benb0201 <111536131+benb0201@users.noreply.github.com>
2023-10-11 16:38:47 +0100

    Initial commit

---
+++ src/main/java/com/example/travelpal/TravelpalApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelpalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travelpal/TravelpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

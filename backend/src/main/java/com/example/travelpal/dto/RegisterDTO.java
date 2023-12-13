698b2b660b0b735d2f847a631a68db9e0bfe697b
benb0201 <111536131+benb0201@users.noreply.github.com>
2023-12-13 18:32:12 +0000

    Adding Spring security and new logic to register and login client(Didn't add all files the first time)

--- backend/src/main/java/com/example/travelpal/dto/RegisterDTO.java
+++ backend/src/main/java/com/example/travelpal/dto/RegisterDTO.java
@@ -1,2 +1,63 @@
-package com.example.travelpal.dto;public class RegisterDTO {
+package com.example.travelpal.dto;
+
+import java.time.LocalDate;
+
+public class RegisterDTO {
+
+    private String name;
+    private String email;
+    private String password;
+    private LocalDate dob;
+
+    public RegisterDTO(String name, String email, String password, LocalDate dob){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.dob = dob;
+    }
+
+    public RegisterDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterDTO{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", dob=" + dob +
+                '}';
+    }
 }
--- backend/src/main/java/com/example/travelpal/response/RegisterResponse.java
+++ backend/src/main/java/com/example/travelpal/response/RegisterResponse.java
@@ -1,2 +1,38 @@
-package com.example.travelpal.response;public class RegisterResponse {
+package com.example.travelpal.response;
+
+public class RegisterResponse {
+    String message;
+    Boolean status;
+
+    public RegisterResponse(String message, Boolean status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public RegisterResponse() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterResponse{" +
+                "message='" + message + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }

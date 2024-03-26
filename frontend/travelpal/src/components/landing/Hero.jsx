58e1747975dc558834be6872c831dbd6f9d280cb
benb0201 <111536131+benb0201@users.noreply.github.com>
2024-03-26 13:45:05 +0000

    Upgrading the landing page

--- frontend/travelpal/src/components/landing/Hero.jsx
+++ frontend/travelpal/src/components/landing/Hero.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
-const Hero = function ({imageSrc}) {
+const Hero = function ({ imageSrc }) {
   return (
     <div className="hero">
-    <img src={imageSrc} alt="Travel" className='hero_image' />
-    <h1 className="hero_title" >Easy travels</h1>  {/* Randomly generated planning quotes */}
+      <img src={imageSrc} alt="Travel" className="hero_image" />
+      <h1 className="hero_title">Easy travels</h1>{" "}
+      {/* Randomly generated planning quotes (future implementation) */}
+      <p className="scroll_prompt">Scroll for more</p> {/* Scroll prompt */}
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- frontend/travelpal/src/components/landing/Slider.jsx
+++ frontend/travelpal/src/components/landing/Slider.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import "./Slider.css";
 import { useInView } from "react-intersection-observer";
+import { useNavigate } from "react-router-dom";
 
 const Slider = ({ imageSrc, title, subtitle, flipped }) => {
   const { ref, inView } = useInView({
     threshold: 0.4,
   });
+  const navigate = useNavigate(); //this function will help me use react router to navigate
 
   const renderContent = () => {
+    const content = (
+      <>
+        <h1 className="slider_title">{title}</h1>
+        <p className="slider_subtitle">{subtitle}</p>
+        <button
+          className="get_started_button"
+          onClick={() => navigate("/login")}
+        >
+          Get Started
+        </button>
+      </>
+    );
+
     if (flipped) {
       return (
         <>
           <img src={imageSrc} alt="Travel" className="slider_image" />
-          <div className="slider_content">
-            <h1 className="slider_title">{title}</h1>
-            <p>{subtitle}</p>
-          </div>
+          <div className="slider_content">{content}</div>
         </>
       );
     } else {
       return (
         <>
-          <div className="slider_content">
-            <h1 className="slider_title">{title}</h1>
-            <p>{subtitle}</p>
-          </div>
+          <div className="slider_content">{content}</div>
           <img src={imageSrc} alt="Travel" className="slider_image" />
         </>
       );
